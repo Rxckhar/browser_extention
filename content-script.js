1b6aea198a78da55173b2a7d0b2cea24f2f68ce1
Сафин Руслан Мухсиддинович <pl8426@PL1.EDU>
2023-03-18 14:30:47 +0500

    save

--- content-script.js
+++ content-script.js
@@ -1,30 +1,9 @@
-// function deleteBlock() {
-//     if(!!document.getElementsByClassName(
-//         "border py-2 graph-before-activity-overview"
-//     )) {
-//         document.getElementsByClassName(
-//             "border py-2 graph-before-activity-overview"
-//         )[0].remove()
-//     }
-// }
-
-// deleteBlock();
-// function paint(){
-//     if(!!document.getElementsByClassName(
-//                 "ContributionCalendar-day"
-//             )) {
-//                 document.getElementsByTagName(
-//                     "data-ix"
-//                 )[0].remove()
-//             }
-    
-// }
-let elements = document.getElementsByClassName("ContributionCalendar-day");
-let colors = ['216e39', '30a14e', '40c463', '9be9a8'];
-let color,
-    opacity;
 
 function changeColor(){
+        let colors = ['216e39', '30a14e', '40c463', '9be9a8'];
+        let color,
+        opacity;
+        let elements = document.getElementsByClassName("ContributionCalendar-day");
         for(let i = 0; i < elements.length; i++){
             function getRandomColor(min, max) {
                 min = Math.ceil(min);
